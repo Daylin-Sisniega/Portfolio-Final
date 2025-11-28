aae56b36a7fb647527bbc4f65c891254ee33a538
Daylin Sisniega <dsisnieg@my.centennialcollege.ca>
2025-11-27 21:30:29 -0500

    Fix CI/CD demo - update files

--- server/express.js
+++ server/express.js
@@ -1,3 +1,29 @@
+// import express from "express";
+// import bodyParser from "body-parser";
+// import cookieParser from "cookie-parser";
+// import compress from "compression";
+// import cors from "cors";
+// import helmet from "helmet";
+
+// // Importar rutas
+// import userRoutes from './routes/user.routes.js'
+// import authRoutes from './routes/auth.routes.js'
+
+// const app = express();
+
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(cookieParser());
+// app.use(compress());
+// app.use(helmet());
+// app.use(cors());
+
+// // Rutas
+// app.use('/', userRoutes)
+// app.use('/', authRoutes)
+
+// export default app;
+
 import express from "express";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
@@ -5,13 +31,36 @@ import compress from "compression";
 import cors from "cors";
 import helmet from "helmet";
 
+// Importar rutas
+import userRoutes from "./routes/user.routes.js";
+import authRoutes from "./routes/auth.routes.js";
+
 const app = express();
 
+// Soporte para JSON y URL-encoded
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Rutas
+app.use("/", userRoutes);
+app.use("/", authRoutes);
+
+// Middlewares de configuración
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(compress());
 app.use(helmet());
 app.use(cors());
+
+//Manejo de errores de autorización (express-jwt)
+app.use((err, req, res, next) => {
+  if (err.name === "UnauthorizedError") {
+    res.status(401).json({ error: err.name + ": " + err.message });
+  } else if (err) {
+    res.status(400).json({ error: err.name + ": " + err.message });
+    console.log(err);
+  }
+});
 
 export default app;
